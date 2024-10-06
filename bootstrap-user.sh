2c7f985ea91f9009b1110d0e465704acdd771549
Zaphodbee <Zaphodbee@gmail.com>
2024-10-05 22:06:56 -0700

    Update bootstrap-user.sh

--- bootstrap-user.sh
+++ bootstrap-user.sh
@@ -3,7 +3,7 @@ mkdir ~/repos
 
 # install yay
 cd ~/repos
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
 yay -S google-chrome
 
